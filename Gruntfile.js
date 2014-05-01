56ea340dd3981f6f0ed39ae3d9627f3094192f48
stephband <stephband@googlemail.com>
2014-05-01 17:40:09 +0200

    Adds continuous contoller converter node and grunt packager.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		concat: {
+			'js/midi.js': [
+				'src/midi.js',
+				'src/midi.log.js',
+				'src/midi.input.js',
+				'src/midi.output.js',
+				'src/midi.filter.js',
+				'src/midi.modify.js',
+				'src/midi.continuous.js',
+				'src/midi.graph.js',
+				'src/midi.out-map.js',
+				'src/midi.out-array.js'
+			]
+		},
+
+		uglify: {
+			'js/midi.min.js': ['js/midi.js']
+		}
+	});
+
+	// Load Our Plugins
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Register Default Task
+	grunt.registerTask('default', ['concat', 'uglify']);
+};
